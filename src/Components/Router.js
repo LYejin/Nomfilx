743d8ec4dd83e4e038216711c7b9ca9a1947d86a
LYejin <vgg789@naver.com>
2021-08-10 23:30:24 +0900

    Learning React

---
+++ src/Components/Router.js
@@ -0,0 +1,22 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+import Home from "Routes/Home";
+import TV from "Routes/TV";
+import Search from "Routes/Search";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default () => (
+  <Router>
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/tv" exact component={TV} />
+      <Route path="/search" exact component={Search} />
+      <Redirect from="*" to="/" />
+    </Switch>
+  </Router>
+);
